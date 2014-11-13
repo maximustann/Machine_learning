e64a31a3ae6b4ad2d808a26e03e96353e64e5e1c
Max <titantbx1989215@gmail.com>
2014-11-14 10:48:55 +1300

    up

--- linear_regression/featureNormalize.R
+++ linear_regression/featureNormalize.R
@@ -1,9 +1,12 @@
-featureNormalize <- function(x, mu, sigma) {
+featureNormalize <- function(x) {
 	x_norm <- x
+
+	mu <<- rep(0, ncol(x))
+	sigma <<- rep(0, ncol(x))
 
 	for(i in 1:ncol(x)){
 		mu[i] <<- mean(x[, i])
-		sigma[i] <<- std(x[, i])
+		sigma[i] <<- sd(x[, i])
 		x_norm[, i] <- (x[, i] - mu[i]) / sigma[i]
 	}
 	x_norm
