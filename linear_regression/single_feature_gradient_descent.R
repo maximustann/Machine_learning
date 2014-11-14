24aea5e60ef78bacac6e6d94bac707ddf4659e3d
Max <titantbx1989215@gmail.com>
2014-11-14 21:31:47 +1300

    up

--- linear_regression/single_feature_gradient_descent.R
+++ linear_regression/single_feature_gradient_descent.R
@@ -1,8 +1,15 @@
-gradient_descent <- function(x, y){
-	temp <- rep(1, length(x))
-	x <- cbind(temp, x)
-	theta <- c(0, 0)
+gradient_descent <- function(x, y, theta){
+	m <- length(y)
 	iterations <- 1500
 	learning_rate <- 0.01
-	costFunction(x, y, theta)
+
+	for(i in 1:iterations){
+		temp <- (x %*% theta - y)
+		theta[1] <- theta[1] - learning_rate * (1 / m) * sum(temp)
+		theta[2] <- theta[2] - learning_rate * (1 / m) * sum(temp * x[, 2])
+		if(iterations %% 150 == 0){
+			abline(theta[1], theta[2], col = "red")
+		}
+	}
+	theta
 }
